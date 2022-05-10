d88cb48468c29609d4fd71f69a01d579c490866c
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-05-10 13:01:04 -0500

    Navegacion y retoque a la pagina

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const btnMenu = document.querySelector('.btn-nav');
+const mainNav = document.querySelector('.main-nav');
+
+btnMenu.addEventListener('click',(e)=>{
+    mainNav.classList.toggle('show-menu')
+})
+
+
+
+
+
+
+
