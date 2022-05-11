4c80fe33ff1a4bc3fe67ed5c359cadae4cf298fa
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-05-11 14:33:14 -0500

    animacion nav y links
    Se crea animacion y nav y se configuran links del nav.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,20 @@
+
+//Boton menu
 const btnMenu = document.querySelector('.btn-nav');
 const mainNav = document.querySelector('.main-nav');
 
 btnMenu.addEventListener('click',(e)=>{
     mainNav.classList.toggle('show-menu')
 })
+
+//Animacion menu al hacer scroll
+const mainHeader = document.querySelector('.main-header .contenedor');
+window.addEventListener('scroll',(e=>{
+    let valorScrollTop = this.scrollY;
+    
+    valorScrollTop<1 ? mainHeader.classList.remove('bg-gray',) :mainHeader.classList.add('bg-gray');
+
+}));
 
 
 
