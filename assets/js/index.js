44845bddff8d27134b71df7009623b47c9f45576
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-05-12 17:21:42 -0500

    Animaciones a las card y menu de navegacion.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,18 +3,72 @@
 const btnMenu = document.querySelector('.btn-nav');
 const mainNav = document.querySelector('.main-nav');
 
-btnMenu.addEventListener('click',(e)=>{
+btnMenu.addEventListener('click', (e) => {
     mainNav.classList.toggle('show-menu')
+    // mainHeader.classList.add('bg-gray')
 })
 
-//Animacion menu al hacer scroll
+//Animacion nav al hacer scroll
+//****************************
 const mainHeader = document.querySelector('.main-header .contenedor');
-window.addEventListener('scroll',(e=>{
+window.addEventListener('scroll', (e => {
+    console.log(e);
     let valorScrollTop = this.scrollY;
-    
-    valorScrollTop<1 ? mainHeader.classList.remove('bg-gray',) :mainHeader.classList.add('bg-gray');
+    console.log(valorScrollTop);
+    valorScrollTop < 1 ? mainHeader.classList.remove('bg-gray',) : mainHeader.classList.add('bg-gray');
 
 }));
+
+//Animacion opciones de nav
+//****************************
+const mainMenuOptions = document.querySelectorAll('.main-menu .menu-item');
+
+console.log(mainMenuOptions);
+
+let mainSections = document.querySelector('main').children;
+mainSections = Array.from(mainSections);
+
+// console.log(typeof mainSections);
+
+options = {
+    root: null,
+    rootMargin: '-50% 0%',
+}
+
+const callbackSections = (entries) => {
+    entries.forEach((entry, i) => {
+        if (entry.isIntersecting) {
+
+            let sectionName = entry.target.id;
+
+            mainMenuOptions.forEach((option, i) => {
+               
+                let optionName = option.firstElementChild.href;
+                optionName = optionName.slice(optionName.indexOf('#')+1);
+
+                option.firstElementChild.classList.remove('color-aqua','zoom-text')
+                option.firstElementChild.classList.add('color-white')
+               
+                if(sectionName == optionName){
+                    option.firstElementChild.classList.remove('color-white');
+                    option.firstElementChild.classList.add('color-aqua','zoom-text');
+                }
+
+            })
+
+        }
+    })
+}
+
+const observerSections = new IntersectionObserver(callbackSections, options);
+
+mainSections.forEach(section => {
+    observerSections.observe(section);
+})
+
+
+// console.log(mainMenuOptions);
+
 
 
 
