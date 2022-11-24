de49ce92f1d64fe3e861eca09eb5675f42771cb5
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-11-24 16:32:46 -0600

    Fixed bug of menu animation.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-let width =window.innerWidth;
+let width = window.innerWidth;
 
 //Boton menu
 const btnMenu = document.querySelector(".btn-nav");
@@ -13,15 +13,6 @@ btnMenu.addEventListener("click", (e) => {
 const mainHeader = document.querySelector(".main-header .contenedor");
 
 window.addEventListener("scroll", (e) => {
-  // let valorScrollTop = this.scrollY;
-
-  // if(valorScrollTop < 1){
-  //   mainHeader.classList.remove("nav-effect");
-
-  // }else{
-  //   mainHeader.classList.add("nav-effect");
-  
-  // }
   mainHeader.classList.add('bg-transparent');
 
   setTimeout(() => {
@@ -32,7 +23,7 @@ window.addEventListener("scroll", (e) => {
 
 //Animacion opciones de nav
 //****************************
-console.log({width});
+console.log({ width });
 
 addEventListener("resize", () => {
   width = window.innerWidth;
@@ -47,40 +38,11 @@ let mainSections = document.querySelector("main").children;
 
 mainSections = Array.from(mainSections);
 
-let options = {}
-addEventListener('resize',()=>{
-  width = window.innerWidth;
-  console.log({width});
+let options = {
+  root: null,
+  threshold: .25
 
-  if(width<768){
-    options = {
-      root: null,
-      threshold: .25
-    
-    };
-  }else{
-    options = {
-      root: null,
-      threshold: .80
-    
-    };
-  }
-})
-
-if(width<768){
-  options = {
-    root: null,
-    threshold: .25
-  
-  };
-}else{
-  options = {
-    root: null,
-    threshold: .80
-  
-  };
-}
-
+};
 
 const callbackSections = (entries) => {
   entries.forEach((entry, i) => {
@@ -120,57 +82,56 @@ const setAnimation = (elementsToAnimate, animationClass, rootMargin) => {
   const options = {
     root: null,
     rootMargin,
-    // threshold: 0.50
   };
   const cbElements = (entries) => {
     entries.forEach((entry) => {
 
       if (entry.isIntersecting) {
         entry.target.classList.add(animationClass)
-      } 
-      
+      }
+
     });
   };
 
   const observerElements = new IntersectionObserver(cbElements, options);
-  elements.forEach(element=>observerElements.observe(element))
+  elements.forEach(element => observerElements.observe(element))
 }
 
 
-if(width<992){
-  setAnimation('.element-from-up','from-up-animation','-10%')
-  setAnimation('.element-from-left','from-left-animation','-10%')
-  setAnimation('.element-from-right','from-right-animation','-10%')
-  setAnimation('.element-from-grow','from-grow-animation','-10%')
+if (width < 992) {
+  setAnimation('.element-from-up', 'from-up-animation', '-10%')
+  setAnimation('.element-from-left', 'from-left-animation', '-10%')
+  setAnimation('.element-from-right', 'from-right-animation', '-10%')
+  setAnimation('.element-from-grow', 'from-grow-animation', '-10%')
 
 
 }
-else{
-  setAnimation('.element-from-up','from-up-animation','-50% 0%')
-  setAnimation('.element-from-left','from-left-animation','-50% 0%')
-  setAnimation('.element-from-right','from-right-animation','-50% 0%')
-  setAnimation('.element-from-grow','from-grow-animation','-50% 0%')
+else {
+  setAnimation('.element-from-up', 'from-up-animation', '-50% 0%')
+  setAnimation('.element-from-left', 'from-left-animation', '-50% 0%')
+  setAnimation('.element-from-right', 'from-right-animation', '-50% 0%')
+  setAnimation('.element-from-grow', 'from-grow-animation', '-50% 0%')
 
 }
-let highOfPage =window.scrollY;
-resetAnimations('element-from-up','from-up-animation',highOfPage);
+let highOfPage = window.scrollY;
+resetAnimations('element-from-up', 'from-up-animation', highOfPage);
 
-window.addEventListener('scroll',()=>{
-  highOfPage =window.scrollY;
-  resetAnimations('element-from-up','from-up-animation',highOfPage)
+window.addEventListener('scroll', () => {
+  highOfPage = window.scrollY;
+  resetAnimations('element-from-up', 'from-up-animation', highOfPage)
 
 
 })
 
-function resetAnimations(animatedElement,classToRemove,highOfPage){
+function resetAnimations(animatedElement, classToRemove, highOfPage) {
   // let high =highOfPage;
 
-  const elements =document.querySelectorAll(`.${animatedElement}`)
+  const elements = document.querySelectorAll(`.${animatedElement}`)
 
-  if(highOfPage===0){
+  if (highOfPage === 0) {
     console.log(highOfPage);
 
-    elements.forEach(element=>element.classList.remove(classToRemove));
+    elements.forEach(element => element.classList.remove(classToRemove));
   }
 
 }
