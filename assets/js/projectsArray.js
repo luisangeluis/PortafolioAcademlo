1735066641c3b74f9f58cb0ae7ffb709e15fe840
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2024-10-10 15:14:02 -0600

    Added function to render projects by JS

---
+++ assets/js/projectsArray.js
@@ -0,0 +1,40 @@
+export const projects = [
+    {
+        title:"the movie db",
+        description:"in this app you can find all the information about movies and tv shows.",
+        technologies:["react","redux","bootstrap","axios","react-router"],
+        links:[
+            {
+                project:"https://tmdb-react-luis.netlify.app/",
+                github:"https://github.com/luisangeluis/TMDBReact"
+            }
+        ],
+        image:"./assets/images/desktop_main-image.png"
+    },
+    {
+        title:"crud of products",
+        description:"example of a crud of products.",
+        technologies:["node","express","nextjs","react","sequelize","mysql"],
+        links:[
+            {
+                project:"https://crud-products-react2.vercel.app/",
+                github:"https://github.com/luisangeluis/CrudProductsReact2"
+            }
+        ],
+        image:"/assets/images/crud2.png"
+
+    },
+    {
+        title:"pokedex",
+        description:"in this app you can find information about your favorite pokemon.",
+        technologies:["react","redux","bootstrap","axios","react-router"],
+        links:[
+            {
+                project:"https://pokedex-luis.netlify.app/",
+                github:"https://github.com/luisangeluis/Pokedex"
+            }
+        ],
+        image:"./assets/images/pokedex.png"
+
+    }
+]
